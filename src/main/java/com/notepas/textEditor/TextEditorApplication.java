80e99a63a3f1ce56ac6268d971535a185eb0c12c
Gaurav <gaurav556pant@gmail.com>
2024-02-17 12:22:14 +0530

    working on a now type of styling in to a front-end

--- src/main/java/com/notepas/textEditor/TextEditorApplication.java
+++ src/main/java/com/notepas/textEditor/TextEditorApplication.java
@@ -1,48 +1,42 @@
 package com.notepas.textEditor;
 
-
-//import java.awt.*;import javax.swing.*;
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//import javafx.application.Platform;
-//import javafx.embed.swing.JFXPanel;
-//import javafx.scene.Scene;
-//import javafx.scene.web.WebView;
-
 @SpringBootApplication
-public class TextEditorApplication{
-//	private final JFXPanel jfxPanel = new JFXPanel();
-
+public class TextEditorApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TextEditorApplication.class, args);
 
-//		SwingUtilities.invokeLater(new Runnable() {
-//			@Override
-//			public void run() {
-//				new TextEditorApplication().setVisible(true);
-//			}
-//		});
+		// Open default web browser
+		openBrowser("http://localhost:9091/notepad/");
 	}
 
-//	public TextEditorApplication() {
-//		super("Notepad Swing App");
-//		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//		setSize(1920, 1080);
-//		setLayout(new BorderLayout());
-//
-//		initJavaFX();
-//
-//		add(jfxPanel, BorderLayout.CENTER);
-//	}
+	private static void openBrowser(String url) {
+		try {
+			if (Desktop.isDesktopSupported()) {
 
-//	private void initJavaFX() {
-//		Platform.runLater(() -> {
-//			WebView webView = new WebView();
-//			webView.getEngine().load("http://localhost:9091/notepad/");
-//
-//			jfxPanel.setScene(new Scene(webView));
-//		});
-//	}
+				Desktop desktop = Desktop.getDesktop();
+				if (desktop.isSupported(Desktop.Action.BROWSE)) {
+					desktop.browse(new URI(url));
+				}
+			} else {
+				// For environments where Desktop is not supported
+				String os = System.getProperty("os.name").toLowerCase();
+
+				if (os.contains("win")) {
+					Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + url);
+				} else if (os.contains("nix") || os.contains("nux") || os.contains("mac")) {
+					Runtime.getRuntime().exec("xdg-open " + url);
+				}
+			}
+		} catch (IOException | URISyntaxException e) {
+			e.printStackTrace();
+		}
+	}
 }
